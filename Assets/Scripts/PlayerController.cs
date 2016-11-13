857213f810db483e9a0fa14d79de1c31e380306d
Gkxd <gkxd@users.noreply.github.com>
2016-11-13 14:54:53 -0500

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (Microphone.devices.Length > 0)
+        {
+            audioSource.clip = Microphone.Start(Microphone.devices[0], true, 1, 44100);
+            audioSource.Play();
+        }
+        else
+        {
+            Debug.LogError("No microphones found");
+        }
+    }
+
+    void Update()
+    {
+        float[] data = new float[44100];
+        audioSource.clip.GetData(data, 0);
+        Debug.Log(Mathf.Max(data));
+
+        float[] spectrum = new float[1024];
+        audioSource.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
+
+        for (int i = 1; i < spectrum.Length - 1; i++)
+        {
+            Debug.DrawLine(new Vector3(i - 1, spectrum[i] * 1000, 0), new Vector3(i, spectrum[i + 1] * 1000, 0), Color.red);
+        }
+    }
+}
